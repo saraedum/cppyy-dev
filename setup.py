844dbad33f309a784c2d1d8f06db0e10691d10ed
Julian Rüth <julian.rueth@fsfe.org>
2021-06-21 15:25:53 +0200

    Allow usage of pre-built dictionary and rootmap when crosscompiling
    i.e., when rootcling binary cannot be invoked on the build system

--- setup.py
+++ setup.py
@@ -253,7 +253,11 @@ class my_install(_install):
 
      # remove allDict.cxx.pch as it's not portable (rebuild on first run, see cppyy)
         log.info('removing allDict.cxx.pch')
-        os.remove(os.path.join(get_prefix(), 'etc', 'allDict.cxx.pch'))
+        allDictPath = os.path.join(get_prefix(), 'etc', 'allDict.cxx.pch')
+        try:
+            os.remove(allDictPath)
+        except FileNotFoundError:
+            print(f"The precompiled header {allDictPath} is not present. This is expected when cross-compiling.")
      # fix-up the standard reported by cling-config (which calls root-config): if C++14
      # was requested, LLVM16 (and thus ROOT) will up it 17; and for manylinux,# which
      # was build with 17, reset the default cxxversion to 20 if no user override
